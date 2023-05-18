6eac7d9f1ce73096389e8169f7e1fd2002883609
Kevinmrozek <kmrozek0859@gmail.com>
2023-05-18 11:01:38 -0500

    semi completed challenge

--- src/index.js
+++ src/index.js
@@ -1,1 +1,49 @@
-// write your code here
+fetch('http://localhost:3000/images')
+    .then(response => response.json())
+    .then((dog) => renderDog(dog))
+fetch('http://localhost:3000/comments')
+    .then(response => response.json())
+    .then((comments) => renderDog(comments))
+
+function renderDog(data) {
+    console.log(data)
+
+    const span = document.querySelector('#like-count')
+    
+
+    const button = document.querySelector('#like-button')
+    button.id = 'like-button'
+
+    button.addEventListener('click', addLikes)
+
+    function addLikes() {
+        data.likes = parseInt(1) + (' likes')
+        span.textContent = `${data.likes}`
+    }
+
+}
+
+
+
+
+
+const form = document.querySelector('#comment-form')
+
+form.addEventListener('submit', (e) => handleSubmit(e))
+
+
+function handleSubmit(e) {
+    e.preventDefault()
+
+    let previousComment = document.querySelector('#comments-list').textContent
+    let newComment = e.target['comment'].value
+
+    let allComments = previousComment + newComment
+
+    const commentBar = document.querySelector('#comments-list')
+
+    commentBar.textContent = allComments
+}
+        
+
+
